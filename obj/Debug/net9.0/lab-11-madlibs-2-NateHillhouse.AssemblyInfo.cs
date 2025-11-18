4a9ac4960c8efe2fbffa59aec00fe4b2ab859b96
mizterwaffles <nhillhouse04@gmail.com>
2025-11-18 12:07:52 -0700

    Added main with file attachment options

--- obj/Debug/net9.0/lab-11-madlibs-2-NateHillhouse.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-11-madlibs-2-NateHillhouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-11-madlibs-2-NateHillhouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f503cb20b4a56bfd77f61906f6013c6091c4865e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9eafd39d149cf84eb8f42da2747046f47e47d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-11-madlibs-2-NateHillhouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-11-madlibs-2-NateHillhouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
